4694d560d30f2d2cf41326d7877e5a6bd68967a9
VladimirChukharev <Vladimir.Chukharev@gmail.com>
2023-05-27 16:55:55 +0400

    Add a stub of a file, only the docstring was changed.

---
+++ parse.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""Read and parse a GEDCOM file creating an object for HEADer, dictionaries of ALBUMs, INDIviduals, FAMilies, SOURces"""
+
+if __name__ == "__main__":
+    pass
